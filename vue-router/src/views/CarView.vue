dbff056e01c417d30a17285da78dd4b0feede6a7
Andrey <boombardir20@gmail.com>
2023-01-05 14:48:53 +0500

    useRouter()

--- vue-router/src/views/CarView.vue
+++ vue-router/src/views/CarView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import {useRoute, RouterView} from "vue-router";
+import {useRoute, useRouter, RouterView} from "vue-router";
 import cars from "../data/cars.json";
 
 const route = useRoute();
 // route.params - объект с параметрами маршрута /cars/:id
+
+const router = useRouter();
+// router.push() - переход на другой маршрут (переход на другую страницу)
 
 // const car = cars.find((car) => car.id === parseInt(route.params.id));
 const car = cars.find((car) => car.id === +route.params.id);
@@ -17,6 +20,7 @@ const {name, price, year} = car;
     <h1>{{ name }}</h1>
     <p>{{ year }}</p>
     <p>{{ price }}</p>
+    <button @click="router.push(`/cars/${+route.params.id}/contact`)">Click for contact</button>
     <RouterView/>
   </div>
 </template>
